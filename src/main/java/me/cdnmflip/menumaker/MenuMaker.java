3b8136fbe890e245372165568406b907889d3f2d
codenameflip <codenameflip@gmail.com>
2018-08-01 17:28:32 -0700

    initial commit

---
+++ src/main/java/me/cdnmflip/menumaker/MenuMaker.java
@@ -0,0 +1,17 @@
+package me.cdnmflip.menumaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MenuMaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
