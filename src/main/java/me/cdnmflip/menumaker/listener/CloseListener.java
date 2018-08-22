3955ce0a6ce77a0b63fadb6c5b047a7ff86f8888
codenameflip <codenameflip@gmail.com>
2018-08-22 03:21:25 -0400

    apply patches from example branch

--- src/main/java/me/cdnmflip/menumaker/listener/CloseListener.java
+++ src/main/java/me/cdnmflip/menumaker/listener/CloseListener.java
@@ -1,7 +1,6 @@
 package me.cdnmflip.menumaker.listener;
 
 import me.cdnmflip.menumaker.MenuMaker;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
